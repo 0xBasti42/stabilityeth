71f6bc7c27a024c0fe6cb7b9e72d1325bd97f09b
Isla-Labs <admin@islalabs.co>
2026-03-04 11:44:23 +0000

    Clean layout

--- contracts/src/omnichain-original/hub/interfaces/ISETHAdapter.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-interface ISETHAdapter {
-    function depositFor(address recipient) external payable returns (bool);
-    function withdrawFor(address recipient, uint256 sethAmount) external;
-    function relayBurnedFees(uint256 sethBurned) external;
-}
